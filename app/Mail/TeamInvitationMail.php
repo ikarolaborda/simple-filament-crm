180753054f1f5a564cc68ac8b55b105343c4a653
Ikaro C. Laborda <iclaborda@msn.com>
2024-04-05 20:26:01 +0100

    feat: implementing e-mail invitation feature

---
+++ app/Mail/TeamInvitationMail.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Invitation;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\URL;
+
+class TeamInvitationMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(
+        private readonly Invitation $invitation
+    )
+    {
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Invitation to join ' . config('app.name'),
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'emails.team-invitation',
+            with: [
+                'acceptUrl' => URL::signedRoute(
+                    "invitation.accept",
+                    ['invitation' => $this->invitation]
+                ),
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
